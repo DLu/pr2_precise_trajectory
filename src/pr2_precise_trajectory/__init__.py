580b224f156bede957c775a3e7e82a022cc61ba0
David V. Lu!! <davidvlu@gmail.com>
2015-03-20 19:37:28 -0500

    Score Checker

--- src/pr2_precise_trajectory/__init__.py
+++ src/pr2_precise_trajectory/__init__.py
@@ -13,12 +13,14 @@ DEFAULT_TIME = 3.0
 def get_time(move):
     return move.get(TIME, DEFAULT_TIME)
 
-def precise_subset(movements, key):
+def precise_subset(movements, key, include_label=False):
     t = 0.0
     L = []
     for move in movements:
         if key in move:
             m = {key: move[key], TIME: get_time(move) + t }
+            if include_label and 'label' in move:
+                m['label'] = move['label']
             L.append(m)
             t = 0.0
         else:
